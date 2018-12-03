827d0f350937079457e20a7800f8536ef8fd63fd
gudangsolusigroup <gudangsolusigroup@gmail.com>
2018-12-03 14:52:20 +0700

    wew

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	@RequestMapping("/test")
+	@RequestMapping("/")
     public String home() {
         // return System.getenv("TEST");
 		return "Hello Docker World";
